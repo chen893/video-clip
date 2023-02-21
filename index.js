394399dec28621c4e6bebed14eaa44615425a78b
chenshuanglong <chenshuanglong@meituan.com>
2023-02-22 01:03:02 +0800

    feat: 多个视频合并

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { readFile } = require("fs/promises");
 const { readdir } = require("fs/promises");
 const path = require("path");
 const ffmpeg = require("fluent-ffmpeg");
+const { mkdir } = require("fs/promises");
+const { existsSync } = require("fs");
 const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
 ffmpeg.setFfmpegPath(ffmpegPath);
 
@@ -23,7 +25,7 @@ function timeTo(time) {
     parseInt(arr[0]) * 60 * 60 +
     parseInt(arr[1]) * 60 +
     parseInt(arr[2]) +
-    parseInt(k) * 0.001;
+    parseInt(k) * 0.01;
   return sum;
 }
 async function startWork() {
@@ -39,7 +41,7 @@ async function startWork() {
         subtitleContent = [];
         const messageArr = data.split("\n");
         messageArr.map((item) => {
-          const message = item.split(",,").pop();
+          const message = item.split(",,").pop().replace("\r", "").trim();
           const time = item.slice(12, 33).split(",");
           // console.log(time[0], timeTo(time[0]));
           // console.log(time[0], time[1])
@@ -68,17 +70,54 @@ async function startWork() {
   }
 
   // console.log(filmCollection);
-  findWordInMovies("爱", "大话西游之月光宝盒");
+  await findWordInMovies("猴子", "大话西游之月光宝盒");
 }
 
-const video = ffmpeg(inputFilePath);
 // const video = ffmpeg(inputFilePath).seekInput(470.069).duration(478.032 - 470.069).output(outputFilePath).on("end", function () {
 //   console.log("file has been processed");
 // }).run();
 
 startWork();
+// ready('波罗蜜') .format('flv');
+async function ready(key) {
+  console.log("进入ready");
+  const inputPath = path.join(__dirname, "output", key);
+  let files = await readdir(inputPath);
+  console.log(files);
+  files = files.map((item) => path.join(inputPath, item));
+  console.log(files);
+  if (!existsSync(path.join("collection"))) {
+    await mkdir(path.join("collection"));
+  }
+  const command = ffmpeg();
+  command.setFfmpegPath(ffmpegPath);
+  files.forEach((clip) => {
+    console.log("---------");
+    console.log("clip", clip);
+    command.input(clip);
+  });
+  try {
 
-function findWordInMovies(key, movie) {
+  const collectionPath = path.join(__dirname, "collection", key + ".mkv");
+  command
+    .mergeToFile(collectionPath,path.join(__dirname, "collection") );
+    // console.log(ffmpeg.concat, 'fn')
+  // ffmpeg.concat(files, collectionPath)
+
+  }
+  catch(err) {
+    console.log(err.message)
+  }
+}
+
+async function findWordInMovies(key, movie) {
+  let target = 0;
+  let success = 0;
+  const isExistDir = existsSync(path.join("output", key));
+
+  if (!isExistDir) {
+    await mkdir(path.join("output", key));
+  }
   let targetMovie = {};
   for (let m of filmCollection) {
     if (m.name === movie) {
@@ -86,23 +125,32 @@ function findWordInMovies(key, movie) {
       break;
     }
   }
-  const {subtitleContent} = targetMovie
+  let time = 1000;
+  const { subtitleContent } = targetMovie;
   for (sub of subtitleContent) {
-
     const { message, startTime, endTime } = sub;
-    
+
     if (message.includes(key)) {
-      console.log(startTime, endTime)
-      video
-        .seekInput(startTime)
-        .duration(endTime - startTime)
-        .output(path.join("output", message + ".mkv"))
-        .on("end", function () {
-          console.log("file has been processed");
-        })
-        .run();
-        break
+      // console.log(startTime, endTime);
+      time = time + 1000;
+      setTimeout(() => {
+        success++;
+
+        const video = ffmpeg(inputFilePath).native();
+        const pro = video
+          .seekInput(startTime)
+          .duration(endTime - startTime)
+          .output(path.join("output", key, message + ".mkv"))
+          .on("end", function () {
+            target++;
+            if (target === success) ready(key);
+            console.log("file has been processed");
+          })
+          .run();
+      }, time);
+
+      // console.log(typeof pro, "pro");
+      // break
     }
-    
-  };
+  }
 }
